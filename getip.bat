524c5a5a9ca18540ef14135e4fa0e34356042925
chinayangh <linhaier@qq.com>
2019-01-01 09:53:01 +0800

    Update getip.bat

--- getip.bat
+++ getip.bat
@@ -1,5 +1,5 @@
 @echo off
-rem for /f "tokens=3"  %%a in ('netsh int ipv4 show add wlan ^| findstr /i ip') do (
+rem for /f "tokens=3"  %%a in ('netsh int ipv4 show add 本地连接 ^| findstr /i ip') do (
 for /f "tokens=3"  %%a in ('netsh int ipv4 show add wlan ^| findstr /i ip') do (
 
  set IP=%%a
