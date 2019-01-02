8961be192b91b50b565547b7ae58d08763d879ca
chinayangh <linhaier@qq.com>
2019-01-02 10:25:40 +0800

    Update 计划任务.bat

--- 计划任务.bat
+++ 计划任务.bat
@@ -1,3 +1,4 @@
 @echo off
+rem https://docs.microsoft.com/en-us/previous-versions/windows/it-pro/windows-server-2012-R2-and-2012/cc725744(v=ws.11)
 schtasks /create /tn log /tr c:\log.bat /sc minute /mo 10  /ru System
 @pause
